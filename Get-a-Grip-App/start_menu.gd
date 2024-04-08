c1c14c17a407cd3d626b9527dae34e5f0d9e622a
Jared Talbot <jared.talbot-2@selu.edu>
2024-04-08 16:09:11 -0500

    feat: semi-finishing touches

--- Get-a-Grip-App/start_menu.gd
+++ Get-a-Grip-App/start_menu.gd
@@ -4,12 +4,18 @@ extends Control
 
 @onready var server_address_entry = $Panel/MenuContainer/ConnectionFormContainer/ServerAddressEntry
 @onready var submit_button = $Panel/MenuContainer/ConnectionFormContainer/SubmitButton
+@onready var subtitle = $Panel/MenuContainer/LogoContainer/Subtitle
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	server_address_entry.text = default_server_address \
 		if WebSocketSettings.server_address == "" \
 		else WebSocketSettings.server_address
+	
+	subtitle.text = "Mobile Controller" \
+		if OS.has_feature("android") or OS.has_feature("ios") \
+		else "Desktop Controller"
 
 func _on_submit_button_pressed():
 	WebSocketSettings.server_address = server_address_entry.text
