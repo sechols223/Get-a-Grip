049159c0b89aa6ee184203a398cbeaefa322b185
Jared Talbot <jared.talbot-2@selu.edu>
2024-04-08 16:36:10 -0500

    feat: update websocket serber

--- WebSocketServer/src/server.ts
+++ WebSocketServer/src/server.ts
@@ -1,6 +1,15 @@
 import { WebSocket, WebSocketServer } from "ws";
 import { PORT } from "./constants";
 
+let nextId = 0;
+
+type WebSocketData = {
+  id: number;
+  [key: string]: any;
+};
+
+const wsMap = new Map<WebSocket, WebSocketData>();
+
 export function setupServer() {
   const wss = new WebSocketServer({ port: PORT });
 
@@ -8,13 +17,21 @@ export function setupServer() {
     ws.on("error", console.error);
 
     ws.on("message", (data, isBinary) => {
-      console.log("received: %s", data);
+      console.log("received from client %d: %s", wsMap.get(ws).id, data);
       wss.clients.forEach((client) => {
         if (client !== ws && client.readyState === WebSocket.OPEN) {
           client.send(data, { binary: isBinary });
         }
       });
     });
+
+    ws.on("close", () => {
+      console.log(`Client ${wsMap.get(ws).id} disconnected`);
+    });
+
+    wsMap.set(ws, { id: nextId++ });
+
+    console.log(`Client ${wsMap.get(ws).id} connected`);
   });
 
   console.log("Web socket server open on port %d!", PORT);
