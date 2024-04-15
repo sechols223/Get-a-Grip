a7d3c3c02ee1cb504ba8098a984fb2452f347e25
Samuel Echols <samuel.echols@selu.edu>
2024-04-14 20:23:08 -0500

    feat: added debug messages

--- src/server.ts
+++ src/server.ts
@@ -4,36 +4,45 @@ import { PORT } from "./constants";
 let nextId = 0;
 
 type WebSocketData = {
-  id: number;
-  [key: string]: any;
+	id: number;
+	[key: string]: any;
 };
 
 const wsMap = new Map<WebSocket, WebSocketData>();
 
 export function setupServer() {
-  const wss = new WebSocketServer({ port: Number(PORT) });
+	const wss = new WebSocketServer({ port: Number(PORT) });
 
-  wss.on("connection", (ws) => {
-    ws.on("error", console.error);
+	wss.on("connection", (ws, req) => {
+		const ip = req.socket.remoteAddress;
+		console.log(`Client on ip: ${ip} connected`);
+		ws.on("error", console.error);
 
-    ws.on("message", (data, isBinary) => {
-      console.log("received from client %d: %s", wsMap.get(ws).id, data);
-      wss.clients.forEach((client) => {
-        if (client !== ws && client.readyState === WebSocket.OPEN) {
-          client.send(data, { binary: isBinary });
-        }
-      });
-    });
+		ws.on("message", (data, isBinary) => {
+			console.log("received from client %d: %s", wsMap.get(ws).id, data);
+			wss.clients.forEach((client) => {
+				if (client !== ws && client.readyState === WebSocket.OPEN) {
+					client.send(data, { binary: isBinary });
+				}
+			});
+		});
+		ws.on("ping", () => {
+			console.log("Server pinged");
+		});
 
-    ws.on("close", () => {
-      console.log(`Client ${wsMap.get(ws).id} disconnected`);
-    });
+		ws.on("connection", () => {
+			console.log("client connected");
+		});
 
-    wsMap.set(ws, { id: nextId++ });
+		ws.on("close", () => {
+			console.log(`Client ${wsMap.get(ws).id} disconnected`);
+		});
 
-    console.log(`Client ${wsMap.get(ws).id} connected`);
-  });
+		wsMap.set(ws, { id: nextId++ });
 
-  console.log("Web socket server open on port %d!", PORT);
-  return wss;
+		console.log(`Client ${wsMap.get(ws).id} connected`);
+	});
+
+	console.log("Web socket server open on port %d!", PORT);
+	return wss;
 }
