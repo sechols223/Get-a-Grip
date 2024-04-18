2dcb5ee0838d1fff20f5081a3d72c56bca9a1720
Jared Talbot <jared.talbot-2@selu.edu>
2024-04-17 19:59:10 -0500

    proxy

---
+++ WebsocketProxy/src/index.ts
@@ -0,0 +1,21 @@
+import { WebSocket, WebSocketServer } from "ws";
+
+const PORT = 8085;
+const TARGET = "wss://get-a-grip.onrender.com";
+
+var wss = new WebSocketServer({ port: PORT });
+
+wss.on("connection", (ws) => {
+  ws.on("error", (e) => console.error("incoming ws:", e));
+
+  const proxyWs = new WebSocket(TARGET);
+  proxyWs.on("open", () => {
+    ws.on("close", () => proxyWs.close());
+    ws.on("message", (data, isBinary) =>
+      proxyWs.send(data, { binary: isBinary })
+    );
+  });
+  proxyWs.on("error", (e) => console.error("proxy ws:", e));
+});
+
+console.log('Proxy to "%s" opened on %d!', TARGET, PORT);
